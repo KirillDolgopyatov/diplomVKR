aedd21c79fb23d26f331fef47c495aa38f35d595
Kirill <daa_ma@mail.ru>
2024-02-12 09:50:32 +0300

    first commit

--- add_tema.py
+++ add_tema.py
@@ -1,143 +1,98 @@
 import sys
 
-import pickle
+import json
 
 from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QLabel, QGridLayout, QLineEdit, QPushButton, \
     QMessageBox
 
-from Designer.designerTest import Ui_MainWindow  # Импорт класса Ui_MainWindow из другого модуля
+from Designer.designerTest import Ui_MainWindow
 
-
-########################################################################################################################
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_MainWindow()  # Создание экземпляра класса Ui_MainWindow
-        self.ui.setupUi(self)  # Инициализация UI элементов
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
         self.layout = QVBoxLayout()
-        self.ui.widget_2.setLayout(self.layout)  # Установка QVBoxLayout как layout для виджета widget_2
-
+        self.ui.widget_2.setLayout(self.layout)
         self.ui.btnSaveTopic.clicked.connect(self.create_new_label)
 
-        self.restore_widgets()
-
-    def create_new_label(self):  # Создание новой метки и вызов функции new_grid
+    def create_new_label(self):
         if self.ui.lineTopic.text() != "":
             text = self.ui.lineTopic.text()
             label = QLabel(text)
             label.setStyleSheet('color: white; font: 14pt;')
             self.layout.addWidget(label)
 
-            self.new_grid()  # Создание нового grid
-
+            self.new_grid()
             self.ui.lineTopic.clear()
         else:
-            QMessageBox.warning(None, "Ошибка", "Введите тему")  # Показ предупреждающего сообщения
+            QMessageBox.warning(None, "Ошибка", "Введите тему")
 
-    def new_grid(self):  # Создание нового grid layout с элементами
+    def new_grid(self):
         grid = QGridLayout()
         self.layout.addLayout(grid)
 
         line_edit = self.create_new_line_edit()
         grid.addWidget(line_edit, 0, 0)
 
-        plus_right = QPushButton()
-        plus_right.setFixedSize(20, 20)
-        plus_right.setIcon(QIcon("icons/iconPlus.svg"))
-        plus_right.setIconSize(QSize(14, 14))
+        self.add_buttons_to_grid(grid, 0, 0)
 
-        plus_down = QPushButton()
-        plus_down.setFixedSize(20, 20)
-        plus_down.setIcon(QIcon("icons/iconPlus.svg"))
-        plus_down.setIconSize(QSize(14, 14))
+    def add_buttons_to_grid(self, grid, row, column):
+        plus_right = self.create_new_button(lambda: self.add_new_element(grid, row, column + 1, 'right'))
+        plus_down = self.create_new_button(lambda: self.add_new_element(grid, row + 1, column, 'down'))
 
-        grid.addWidget(plus_right, 0, 1)
-        grid.addWidget(plus_down, 1, 0)
-
-        plus_right.clicked.connect(lambda: self.create_new_btn_right(grid))
-        plus_down.clicked.connect(lambda: self.create_new_btn_down(grid))
-
-        self.column_stretch(grid)  # Установка растягиваем колонки и строки
-
-    def create_new_btn_down(self, grid):  # Создание новой строки при нажатии на кнопку вниз
-        clicked_widget = self.sender()
-
-        # Получение позиции нажатого виджета в сетке
-        for i in range(grid.count()):
-            item = grid.itemAt(i)
-            if item.widget() == clicked_widget:
-                row, column = grid.getItemPosition(i)[:2]
-                break
-        else:
-            return
-
-        line_edit = self.create_new_line_edit()
-        plus_right = self.create_new_button(lambda: self.create_new_btn_right(grid))
-        plus_down = self.create_new_button(lambda: self.create_new_btn_down(grid))
-
-        grid.addWidget(line_edit, row, column)
+        grid.addWidget(plus_right, row, column + 1)
         grid.addWidget(plus_down, row + 1, column)
-        grid.addWidget(plus_right, row, column + 1)
-
-        self.save_state()
-
-    def create_new_btn_right(self, grid):  # Создание новой колонки при нажатии на кнопку вправо
-        clicked_widget = self.sender()
-
-        # Получение позиции нажатого виджета в сетке
-        for i in range(grid.count()):
-            item = grid.itemAt(i)
-            if item.widget() == clicked_widget:
-                row, column = grid.getItemPosition(i)[:2]
-                break
-        else:
-            return
-
-        line_edit = self.create_new_line_edit()
-        plus_right = self.create_new_button(lambda: self.create_new_btn_right(grid))
-
-        grid.addWidget(line_edit, row, column)
-        grid.addWidget(plus_right, row, column + 1)
 
         self.column_stretch(grid)
 
-        self.save_state()
+    def add_new_element(self, grid, row, column, direction):
+        line_edit = self.create_new_line_edit()
+        grid.addWidget(line_edit, row, column)
 
-    def restore_widgets(self):
-        try:
-            with open("widget_state.txt", "r") as file:
-                for line in file:
-                    widget_type, row, column = line.strip().split(',')
-                    if widget_type == "LineEdit":
-                        self.add_line_edit_at_position(int(row), int(column))
-                    elif widget_type == "Button":
-                        self.add_button_at_position(int(row), int(column))
-        except FileNotFoundError:
-            pass  # No saved state, start fresh
+        if direction == 'right':
+            self.add_buttons_to_grid(grid, row, column)
+        else:
+            self.add_buttons_to_grid(grid, row, column)
+
+        self.column_stretch(grid)
 
     def save_state(self):
-        widgets = []
+        layout_data = []
         for i in range(self.layout.count()):
-            widget = self.layout.itemAt(i).widget()
-            if isinstance(widget, QLineEdit):
-                widgets.append(('QLineEdit', widget.text()))
-            elif isinstance(widget, QPushButton):
-                # Assuming you want to save QPushButton without text or with a specific identifier
-                widgets.append(('QPushButton', 'plus'))
-        with open('saveData/widgets_state.pickle', 'wb') as file:
-            pickle.dump(widgets, file)
+            item = self.layout.itemAt(i)
+            grid = item.layout()
+            grid_data = {'widgets': []}
+            for j in range(grid.count()):
+                widget = grid.itemAt(j).widget()
+                if isinstance(widget, QLineEdit):
+                    widget_data = {'type': 'QLineEdit', 'text': widget.text(), 'row': grid.getItemPosition(j)[0],
+                                   'column': grid.getItemPosition(j)[1]}
+                elif isinstance(widget, QPushButton):
+                    # Здесь можно добавить дополнительные свойства кнопок, если это необходимо
+                    widget_data = {'type': 'QPushButton', 'row': grid.getItemPosition(j)[0],
+                                   'column': grid.getItemPosition(j)[1]}
+                grid_data['widgets'].append(widget_data)
+            layout_data.append(grid_data)
+
+        with open('layout_state.json', 'w') as f:
+            json.dump(layout_data, f)
+
+    def closeEvent(self, event):
+        self.save_state()
+        event.accept()  # Подтверждаем закрытие приложения
 
     @staticmethod
-    def column_stretch(grid):  # Установка растягиваем колонки и строки сетки
+    def column_stretch(grid):
         grid.setColumnStretch(grid.columnCount(), 1)
         grid.setRowStretch(grid.rowCount(), 1)
 
     @staticmethod
-    def create_new_line_edit():  # Создание нового QLineEdit
+    def create_new_line_edit():
         line_edit = QLineEdit()
         line_edit.setFixedSize(30, 30)
         line_edit.setStyleSheet("background-color: white; color: black; font: 10pt;")
@@ -145,7 +100,7 @@ class MainWindow(QMainWindow):
         return line_edit
 
     @staticmethod
-    def create_new_button(callback):  # Создание новой кнопки
+    def create_new_button(callback):
         button = QPushButton()
         button.setFixedSize(20, 20)
         button.setIcon(QIcon("icons/iconPlus.svg"))
@@ -153,8 +108,6 @@ class MainWindow(QMainWindow):
         button.clicked.connect(callback)
         return button
 
-
-########################################################################################################################
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
